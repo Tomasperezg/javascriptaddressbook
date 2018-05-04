e504e913f42c051be45dde5a29b4b21c66ac2100
tomas <perezgantomas@gmail.com>
2018-05-04 16:57:45 -0400

    new update!!(not done yet)

--- addressbook.js
+++ addressbook.js
@@ -12,49 +12,63 @@ class Contact {
 class Addressbook {
   constructor(){
     this.contacts = [];
-    let tomas = new Contact("jimmy", "jymmy@email.com", "432-234-3456", "friend");
-    this.contacts[0] = tomas;
+    let jimmy = new Contact("jimmy", "jymmy@email.com", "432-234-3456", "friend");
+    this.contacts[0] = jimmy;
     let billy = new Contact("billy", "billy@gmail.com", "180-234-billy", "childhood friend");
     this.contacts[1] = billy;
 
     }
 
    Add(){
-    let name = prompt("enter your name?");
-    let email = prompt("enter your email?");
-    let phone = prompt("enter phone number?");
-    let relationship = prompt("what is your relationship");
+    let name = document.getElementById("Name").Submit;
+    let email = document.getElementById("Email").Submit;
+    let phone = document.getElementById("Phone").Submit;
+    let relationship = document.getElementById("Relation").Submit;
 
-    let newFriend = new Contact(name,email,phone,relationship);
-    this.contacts.push(newFriend);
+    // function Contact () {
+    //     document.getElementById("??").submit();
+    // let newFriend = new Contact(name,email,phone,relationship);
+    // this.contacts.push(newFriend);
+
   }
 }
 let book = new Addressbook();
-// console.log(book.contacts);
-// book.Add();
-// console.log(book.contacts);
+console.log(book.contacts);
+book.Add();
+console.log(book.contacts);
 
-while(true){
-  let input = prompt("do you wish to add(a) or quit(q) delete(d) or print(p)?");
+// while(true){
+  //let input = prompt("do you wish to add(a) or quit(q) delete(d) or print(p)?");
   if(input === "a"){
     book.Add();
   }
-  else if(input === "q"){
+   else if(input === "q"){
     console.log("goodbye");
-    break;
+    // break;
   }else if(input === "p"){
     console.log(book.contacts);
-    break;
+    // break;
   }
   else if(input === "d"){
     let choice = prompt("What item would you like to remove? EX:1,2,3..?")
     book.contacts.splice(2, choice);
     console.log(book.contacts);
-    break;
+    // break;
 
   }
   else{
     console.log("sorry i dont understand you!");
     console.log("try again");
   }
-}
+// }
+
+// let tagSelection = document.querySelectorAll("form");
+// element.addEventListener("onclick", handlerFunction);
+// "use strict";
+// const buttonClick = document.getElementsByTagName("button");
+// buttonClick[0].addEventListener("click", () => {
+//     const newPara = document.createElement("form");
+//     // const text = document.createTextNode("You did it!");
+//     newPara.appendChild(text);
+//     document.body.appendChild(newPara);
+// });
